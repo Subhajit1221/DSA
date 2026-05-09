443222c304178868a7a4dff60f8adece5f5aa771
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-05-09 16:32:45 +0530

    Time: 23 ms (92.56%) | Memory: 81 MB (19.86%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,74 +1,40 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
 
-        Stack<Integer> s= new Stack<>();
+        int n = heights.length;
 
-        int n=heights.length;
+        Deque<Integer> s = new ArrayDeque<>();
 
-        int nextsmallleft[]=new int[n];
+        int max = 0;
 
-        s.push(0);
-        nextsmallleft[0]=-1;
+        for(int i = 0; i <= n; i++) {
 
-       for(int i=1;i<n;i++){
-        while(!s.isEmpty() && heights[i] <= heights[s.peek()]){
-            s.pop();
+            int currHeight = (i == n) ? 0 : heights[i];
+
+            while(!s.isEmpty() && currHeight < heights[s.peek()]) {
+
+                int height = heights[s.pop()];
+
+                int rightBoundary = i;
+
+                int leftBoundary;
+
+                if(s.isEmpty()) {
+                    leftBoundary = -1;
+                } else {
+                    leftBoundary = s.peek();
+                }
+
+                int width = rightBoundary - leftBoundary - 1;
+
+                int area = height * width;
+
+                max = Math.max(max, area);
+            }
+
+            s.push(i);
         }
 
-        if(s.empty()){
-            nextsmallleft[i]=-1;
-        }else{
-            nextsmallleft[i]=s.peek();
-        }
-
-        s.push(i);
-
-       }
-
-       while(!s.isEmpty()){
-          s.pop();
-       }
-
-       
-
-       
-        int nextsmallright[]=new int[n];
-
-        s.push(n-1);
-        nextsmallright[n-1]=n;
-
-       for(int i=n-2;i>=0;i--){
-        while(!s.isEmpty() && heights[i] <= heights[s.peek()]){
-            s.pop();
-        }
-
-        if(s.empty()){
-            nextsmallright[i]=n;
-        }else{
-            nextsmallright[i]=s.peek();
-        }
-
-        s.push(i);
-
-       }
-
-       int max=0;
-
-       for(int i=0;i<n;i++){
-        
-        int width= nextsmallright[i]-nextsmallleft[i]-1;
-         
-          int current=heights[i]*width;
-
-          if(max<current){
-            max=current;
-          }
-        
-       }
-       
-        
-        
-      return max;
-        
+        return max;
     }
 }
