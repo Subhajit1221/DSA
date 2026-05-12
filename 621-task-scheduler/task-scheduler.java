937c86929ca492555537ceb3dfa0857a3666bb82
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-05-13 03:37:52 +0530

    Time: 8 ms (60.01%) | Memory: 47.5 MB (95.94%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,50 +1,46 @@
+import java.util.*;
+
 class Solution {
     public int leastInterval(char[] tasks, int n) {
 
-        int size=tasks.length;
-        int count=0;
+        int size = tasks.length;
+        int count = 0;
 
-        Integer freq []= new Integer[26];
-        
-        for(int i=0;i<26;i++){
-    freq[i]=0;
-}
-        
-        for(int i=0;i<size;i++) {
+        Integer freq[] = new Integer[26];
 
-            if(freq[tasks[i]-'A']==0){
+        // initialize with 0
+        for(int i = 0; i < 26; i++){
+            freq[i] = 0;
+        }
+
+        // frequency count
+        for(int i = 0; i < size; i++){
+
+            if(freq[tasks[i] - 'A'] == 0){
                 count++;
             }
 
-            freq[tasks[i]-'A']++;
+            freq[tasks[i] - 'A']++;
         }
 
+        // sort in decreasing order
+        Arrays.sort(freq, Collections.reverseOrder());
 
-        
+        // create empty slots
+        int slot = (freq[0] - 1) * n;
 
+        // fill slots using remaining tasks
+        for(int i = 1; i < count; i++){
 
-      Arrays.sort(freq, Collections.reverseOrder());
+            slot = slot - Math.min(freq[i], freq[0] - 1);
+        }
 
-      int interval=0;
-      int slot=(freq[0]-1)*n;;
-
-
-      for(int i=1;i<count;i++){
-           
-           if(slot<=0){
+        // if all slots filled
+        if(slot <= 0){
             return size;
-           }else{
-           slot = slot - Math.min(freq[i], freq[0]-1);
-           }
-      }
+        }
 
-      if(slot<=0){
-            return size;
-           }
-
-
-
-         interval=size+slot;
-         return interval;
+        // remaining slots are idle intervals
+        return size + slot;
     }
 }
