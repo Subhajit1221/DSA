376eb597d061e414a2c3d96bfa0491db10861508
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-08-22 23:45:44 +0530

    Time: 4 ms (52.21%) | Memory: 12.4 MB (64.71%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -1,29 +1,22 @@
 class Solution(object):
     def areSimilar(self, mat, k):
-        
-        mat1 = []
 
-        for row in mat:
-            mat1.append(row[:])
+        m = len(mat)
+        n = len(mat[0])
 
-        i = 0 
-        m=len(mat)
-        n=len(mat[0])
-        while(k!=0):
-            while(i<m):
-                if (i%2==0):
-                    mat[i].append(mat[i].pop(0))
-                else:
-                    mat[i].insert(0,mat[i].pop(n-1))
-            
-                i=i+1
-            k-=1
-            i=0
+        k = k % n
 
-          
+        for i in range(m):
 
-        if(mat1==mat):
-            return True
-        else:
-            return False
-        
+            if i % 2 == 0:
+                # even row -> left shift
+                shift = k
+            else:
+                # odd row -> right shift
+                shift = n - k
+
+            for j in range(n):
+                if mat[i][j] != mat[i][(j + shift) % n]:
+                    return False
+
+        return True
