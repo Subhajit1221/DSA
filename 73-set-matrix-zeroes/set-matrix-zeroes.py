f71f8a98b44b1dc679cce39fe5eca66053ae22ca
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-08-23 14:27:20 +0530

    Time: 615 ms (5.09%) | Memory: 14 MB (6.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -2,21 +2,27 @@ class Solution(object):
     def setZeroes(self, matrix):
 
        
-        row=[]
-        column=[]
+       
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if(matrix[i][j]==0):
-                    row.append(i)
-                    column.append(j)
+                    # matrix[i][j] = float('inf')
+                    for x in  range(len(matrix)):
+                        if(matrix[x][j]!=0):
+                            matrix[x][j]= float('inf')
+                    for y in  range(len(matrix[0])):
+                        if(matrix[i][y]!=0):
+                            matrix[i][y]= float('inf')
+                
+
                 
 
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if i  in row or j in column:
-                   matrix[i][j]=0
+                if(matrix[i][j]==float('inf')):
+                    matrix[i][j] = 0
         
 
 
