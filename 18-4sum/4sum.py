f44b5bb6c2cb2dc3bdbb38ad8fadf913599a2520
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-08-24 23:21:49 +0530

    Time: 526 ms (70.57%) | Memory: 12.3 MB (94.01%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,41 +1,53 @@
 class Solution(object):
     def fourSum(self, nums, target):
-        size=len(nums)
-        left=2
-        right=size-1
-
-        
-        final=[]
-        curr=[]
-       
+        size = len(nums)
+        final = []
 
         nums.sort()
-        
-        for i in range(size-3):
-            for j in range(i+1,size-2):
-                left=j+1
-                right=size-1
-                
-                while left<right:
-                    total=nums[i] + nums[j] + nums[left] + nums[right]
 
-                    if total==target:
-                        curr.append(nums[i])
-                        curr.append(nums[j])
-                        curr.append(nums[left])
-                        curr.append(nums[right])
+        for i in range(size - 3):
 
-                        if curr not in final:
-                            final.append(curr)
-                        
-                        curr=[]
-                    
+            # Skip duplicate i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, size - 2):
+
+                # Skip duplicate j
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = size - 1
+
+                while left < right:
+
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+
+                        final.append([
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        ])
+
+                        # Skip duplicate left values
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+
+                        # Skip duplicate right values
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
                         left += 1
                         right -= 1
-                    
-                    elif total>target:
-                        right-=1
-                    
+
+                    elif total > target:
+                        right -= 1
+
                     else:
-                        left+=1
+                        left += 1
+
         return final
