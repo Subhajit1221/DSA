7e013707cc6c68bd08811eb6cb5a644e1d92e571
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-08-27 21:22:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    
+    public static int lower=0;
+    
+    public static int find(int arr[],int x, int i, int j){
+
+        if(i>j){
+            return lower;
+        }
+
+
+
+
+        int mid=(i+j)/2;
+
+        if(arr[mid]>=x){
+            lower=Math.min(lower,mid);
+            return find(arr,x,i,lower-1);
+        }
+
+        
+
+        return find(arr,x,mid+1,j);
+
+
+    }
+    int lowerBound(int[] arr, int target) {
+        
+        lower=arr.length;
+        
+        
+        return find(arr,target,0,lower-1);
+        
+        
+    }
+}
