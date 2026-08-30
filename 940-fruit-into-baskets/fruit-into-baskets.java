4fb97cf66206aa7a59cb8209650feafe720905ba
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-08-31 00:56:50 +0530

    Time: 7 ms (96.86%) | Memory: 61.7 MB (99.70%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,37 +1,32 @@
 class Solution {
     public int totalFruit(int[] fruits) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] count = new int[fruits.length + 1];
 
         int low = 0;
-        int high = 0;
+        int types = 0;
         int max = 0;
 
-        while (high < fruits.length) {
+        for (int high = 0; high < fruits.length; high++) {
 
-            // Add the current fruit
-            if (map.containsKey(fruits[high])) {
-                map.put(fruits[high], map.get(fruits[high]) + 1);
-            } else {
-                map.put(fruits[high], 1);
+            if (count[fruits[high]] == 0) {
+                types++;
             }
 
-            high++;
+            count[fruits[high]]++;
 
-            // Too many fruit types
-            while (map.size() > 2) {
+            while (types > 2) {
 
-                map.put(fruits[low], map.get(fruits[low]) - 1);
+                count[fruits[low]]--;
 
-                if (map.get(fruits[low]) == 0) {
-                    map.remove(fruits[low]);
+                if (count[fruits[low]] == 0) {
+                    types--;
                 }
 
                 low++;
             }
 
-            // Window now has at most 2 types
-            max = Math.max(max, high - low);
+            max = Math.max(max, high - low + 1);
         }
 
         return max;
