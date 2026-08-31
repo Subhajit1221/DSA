ef133c54fd235a6a880dfe42b3a39d0ae65d4eec
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-08-31 19:09:53 +0530

    Time: 7 ms (89.03%) | Memory: 46.2 MB (65.00%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,13 +1,16 @@
 class Solution {
+
     public int characterReplacement(String s, int k) {
+
+        int n = s.length();
 
         int[] freq = new int[26];
 
         int low = 0;
-        int maxFrequency = 0;
         int maxAnswer = 0;
+        int maxFrequency = 0;
 
-        for (int high = 0; high < s.length(); high++) {
+        for (int high = 0; high < n; high++) {
 
             freq[s.charAt(high) - 'A']++;
 
@@ -16,7 +19,7 @@ class Solution {
                 freq[s.charAt(high) - 'A']
             );
 
-            while ((high - low + 1) - maxFrequency > k) {
+            while (high - low + 1 - maxFrequency > k) {
 
                 freq[s.charAt(low) - 'A']--;
 
