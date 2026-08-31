e3d0e5852b1885e135e2528700bdb4fba51bb96d
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-08-31 09:05:53 +0530

    Time: 17 ms (50.17%) | Memory: 47.8 MB (41.78%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -6,34 +6,29 @@ class Solution {
         }
 
         int c[] = new int[128];
-        
-        int low=0;
-        int high=0;
-        int max=Integer.MIN_VALUE;
 
-        while(high<s.length()){
-            
-                if(c[s.charAt(high)]==0){
-                    c[s.charAt(high)]=1;
-                    high++;
-                    
-                }else{
+        int low = 0;
+        int high = 0;
+        int max = Integer.MIN_VALUE;
 
-                    while(c[s.charAt(high)] == 1){
-                        c[s.charAt(low)]=0;
-                        low++;
-                    }
+        while (high < s.length()) {
 
+            if (c[s.charAt(high)] == 0) {
+
+                c[s.charAt(high)] = 1;
+                high++;
+
+            } else {
+
+                while (c[s.charAt(high)] == 1) {
+                    c[s.charAt(low)] = 0;
+                    low++;
                 }
+            }
 
-                max=Math.max(max,high-low);
-                
-
-               
-            
+            max = Math.max(max, high - low);
         }
 
         return max;
-
     }
 }
