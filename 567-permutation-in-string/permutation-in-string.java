e1dee88529bab2aa39712496dfa73af5fd5e33d3
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-09-02 00:12:00 +0530

    Time: 12 ms (31.52%) | Memory: 44.2 MB (47.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -4,12 +4,11 @@ class Solution {
 
         for (int i = 0; i < 256; i++) {
 
-            if (b[i] > 0) {
-
-                if (a[i] < b[i]) {
+            
+                if (a[i] != b[i]) {
                     return false;
                 }
-            }
+            
         }
 
         return true;
@@ -24,32 +23,31 @@ class Solution {
         int high = 0;
         int low = 0;
 
-        int s1f[] = new int[256];
         int s2f[] = new int[256];
+        int s1f[] = new int[256];
 
-        // Frequency of s1
         for (int i = 0; i < s1.length(); i++) {
             s1f[s1.charAt(i)]++;
         }
 
         while (high < s2.length()) {
 
-            // Add current character
             s2f[s2.charAt(high)]++;
 
-            // If checker becomes true, try shrinking
-            while (checker(s2f, s1f)) {
+            while (high - low + 1 > s1.length()) {
 
-                // If window size == s1 length,
-                // then it is exactly a permutation
-                if (high - low + 1 == s1.length()) {
-                    return true;
-                }
+               
 
-                // Shrink from left
                 s2f[s2.charAt(low)]--;
                 low++;
+
+                
+
             }
+
+             if (checker(s2f,s1f)) {
+                    return true;
+                }
 
             high++;
         }
