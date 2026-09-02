d09ce0b3ac2238713e892cb999cdee4c3c6ad73e
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-09-03 01:06:12 +0530

    Time: 1 ms (77.99%) | Memory: 42.1 MB (88.94%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,32 @@
+class Solution {
+    int srq(int n){
+
+        int sum=0;
+
+        while(n!=0){
+          int d=n%10;
+          sum=sum+d*d;
+          n=n/10;
+        }
+
+        return sum;
+    }
+    public boolean isHappy(int n) {
+
+        int slow=n;
+        int fast=n;
+        
+        while(fast!=1){
+
+            slow=srq(slow);
+            fast=srq(srq(fast));
+
+            if(slow==fast && fast!=1){
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
