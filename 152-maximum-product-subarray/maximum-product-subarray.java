c6f47a57ff2140846e979b9f7ee3701c5ff4d9e4
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-09-05 00:31:03 +0530

    Time: 2 ms (72.08%) | Memory: 47.3 MB (53.54%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -3,10 +3,11 @@ class Solution {
 
         int min=nums[0];
         int max=nums[0];
+        int n=nums.length;
 
         int bestending=nums[0];
 
-        for(int i=1;i<nums.length;i++){
+        for(int i=1;i<n;i++){
             int v1=bestending*nums[i];
             int v2=nums[i];
             int v3=min*nums[i];
