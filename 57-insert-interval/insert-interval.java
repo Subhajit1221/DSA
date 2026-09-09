8e6cccd9e367b19cfbfe195c0244503ee7e9bc1d
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-09-09 10:57:45 +0530

    Time: 1 ms (98.09%) | Memory: 47.1 MB (55.87%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,42 +1,56 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+
+        int arr[][]=new int[intervals.length+1][2];
+
+        int j=0;
+        int flag=0;
+
+        
+
+        for(int i=0;i<intervals.length+1;i++){
+
+            if(j< intervals.length && intervals[j][0]<newInterval[0] ){
+
+            
+
+            arr[i][0]=intervals[j][0];
+            arr[i][1]=intervals[j][1];
+            j++;
+        }else if(flag==0){
+            
+            arr[i][0]=newInterval[0];
+            arr[i][1]=newInterval[1];
+            flag=1;
+        }else{
+            arr[i][0]=intervals[j][0];
+            arr[i][1]=intervals[j][1];
+            j++;
+        }
+
+        }
 
         ArrayList<int[]> list = new ArrayList<>();
 
-        int[][] arr = new int[intervals.length + 1][];
+        int i=0;
 
-    for (int i = 0; i < intervals.length; i++) {
-          arr[i] = intervals[i];
-    }
+        while(i<arr.length){
+            int start= arr[i][0];
 
-    arr[intervals.length] = newInterval;
+            int end= arr[i][1];
 
+            while(i< arr.length-1 && end>=arr[i+1][0]){
 
-Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
-
-
- int i = 0;
-
-        while (i < arr.length) {
-
-            int start = arr[i][0];
-            int end = arr[i][1];
-
-            
-            while (i< arr.length-1 &&
-                   end >= arr[i + 1][0]) {
-
+                end=Math.max(end,arr[i+1][1]);
                 i++;
-
-                end = Math.max(end, arr[i][1]);
             }
 
-            list.add(new int[]{start, end});
-
+            list.add(new int []{start,end});
             i++;
         }
+        
 
         return list.toArray(new int[list.size()][]);
-        
     }
 }
