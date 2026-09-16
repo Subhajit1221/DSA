9f008941eac7fb01bb702bd7d02602b27ae92f32
Subhajit Dutta <120464009+Subhajit1221@users.noreply.github.com>
2026-09-17 00:48:27 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (35.11%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -0,0 +1,84 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode reverse(ListNode temp){
+
+        
+
+        ListNode prev=null;
+        ListNode curr=temp;
+
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        return prev;
+
+        
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        if(head==null || k==1){
+            return head;
+        }
+
+
+        ListNode dummy=new ListNode(0);
+
+        dummy.next=head;
+
+        ListNode prevgrpend=dummy;
+        ListNode grpsrt;
+        ListNode kth;
+        ListNode nextgrp=null;
+
+        
+
+        while(prevgrpend.next != null){
+
+            grpsrt = prevgrpend.next;
+            kth= grpsrt;
+            int count=0;
+            while(kth != null && count<k-1){
+               kth=kth.next;
+               count++;
+            }
+
+            if(kth!=null){
+                nextgrp=kth.next;
+
+                kth.next=null;
+
+                prevgrpend.next=reverse(grpsrt);
+
+                grpsrt.next=nextgrp;
+
+                prevgrpend=grpsrt;
+
+                kth=nextgrp;
+
+                
+            }else{
+                return dummy.next;
+            }
+        }
+
+
+        return dummy.next;
+
+        
+        
+    }
+}
